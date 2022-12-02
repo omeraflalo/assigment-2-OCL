d5456ddf49cefee77ab5eb1a2e9426863deb19b2
omer aflalo <omer.aflalo@gmail.com>
2022-12-02 13:09:26 +0200

    implement constraint 4

--- src/Group.java
+++ src/Group.java
@@ -1,21 +1,24 @@
 import java.util.HashSet;
+import java.util.Set;
 
-public class Group implements  ITestable{
-    private int groupId;
+public class Group implements ITestable {
     HashSet<Hotel> hotels;
+    private int groupId;
 
-    public Group(int id){
+    public Group(int id) {
         hotels = new HashSet<Hotel>();
         groupId = id;
     }
 
-
-
-    public void addHotelToGroup(Hotel hotel){
-        hotels.add(hotel);
+    public static boolean checkAllIntancesConstraints(Model model) {
+        return true;
     }
 
     //getters
+
+    public void addHotelToGroup(Hotel hotel) {
+        hotels.add(hotel);
+    }
 
     public int getGroupId() {
         return groupId;
@@ -25,11 +28,11 @@ public class Group implements  ITestable{
         return hotels;
     }
 
-    private boolean constraint1(){
-        for(Hotel h1 : hotels){
-            for (Hotel h2: hotels){
-                if(h1 != h2){
-                    if(h1.getCity().equals(h2.getCity())){
+    private boolean constraint1() {
+        for (Hotel h1 : hotels) {
+            for (Hotel h2 : hotels) {
+                if (h1 != h2) {
+                    if (h1.getCity().equals(h2.getCity())) {
                         return false;
                     }
                 }
@@ -37,11 +40,27 @@ public class Group implements  ITestable{
         }
         return true;
     }
+
+    private boolean sameServices(Set<Service> serviceSet1, Set<Service> serviceSet2) {
+        return serviceSet1.containsAll(serviceSet2) && serviceSet2.containsAll(serviceSet1);
+    }
+
+    private boolean constraint4() {
+        if (hotels.size() == 0) {
+            return true;
+        }
+        Hotel h = (Hotel) hotels.toArray()[0];
+        Set<Service> serviceSet = h.getServices().keySet();
+        for (Hotel hotel : hotels) {
+            if (!sameServices(serviceSet, hotel.getServices().keySet())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     @Override
     public boolean checkConstraints() {
-       return constraint1();
-    }
-    public static boolean checkAllIntancesConstraints(Model model){
-        return true;
+        return constraint1() && constraint4();
     }
 }
